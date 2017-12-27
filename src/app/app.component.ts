582010572737638dd10c331d4118af043c113067
GLOBANT\matias.luzardi <matias.luzardi@globant.com>
2017-12-27 16:49:06 -0300

    progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,6 @@ import { Meta, Title } from '@angular/platform-browser';
 @Component({
   selector: 'app-root',
   template: `
-  <h1>Universal Demo using Angular and Angular CLI</h1>
-  <a class="col-sm-4" routerLink="/">Home</a>
-  <a routerLink="/lazy">Lazy</a>
-  <a routerLink="/lazy/nested">Lazy_Nested</a>
   <router-outlet></router-outlet>
   `,
   styles: []
--- src/app/services/articles/articles.service.ts
+++ src/app/services/articles/articles.service.ts
@@ -12,11 +12,11 @@ export class ArticlesService {
     let o1 = new News();
     o1.description = "asd";
     o1.id=2222;
-    o1.title="iaojsdioasjdoasjdoasd";
-    o1.description="deadaedaedaede";
-    o1.pictureUrl="asdasdasdasdas";
-    o1.tagName = "asda";
-    o1.webUrl="asdasdasdas";
+    o1.title="Liverpool oficializó a Virgil van Dijk y pagaría... ¡100 millones de dólares!";
+    o1.description="Los 'Reds' confirmaron la adquisición del defensa neerlandés procedente del Southampton";
+    o1.pictureUrl="http://cdn.foxsportsla.com/sites/foxsports-la/files/img/notes/news/Virgil-van-Dijk-Liverpool-271217.jpg";
+    o1.tagName = "Premier League";
+    o1.webUrl= "http://www.foxsports.com.gt/news/337214?origin=app";
     
     return [
        o1,

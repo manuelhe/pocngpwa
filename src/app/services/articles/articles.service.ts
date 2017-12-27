d05d0c99f39fc2d874a40afd24e9e5abe5ad0296
GLOBANT\matias.luzardi <matias.luzardi@globant.com>
2017-12-27 17:12:33 -0300

    CSS

--- src/app/services/articles/articles.service.ts
+++ src/app/services/articles/articles.service.ts
@@ -12,6 +12,7 @@ export class ArticlesService {
     let o1 = new News();
     o1.description = "asd";
     o1.id=2222;
+    o1.date = new Date();
     o1.title="Liverpool oficializó a Virgil van Dijk y pagaría... ¡100 millones de dólares!";
     o1.description="Los 'Reds' confirmaron la adquisición del defensa neerlandés procedente del Southampton";
     o1.pictureUrl="http://cdn.foxsportsla.com/sites/foxsports-la/files/img/notes/news/Virgil-van-Dijk-Liverpool-271217.jpg";
